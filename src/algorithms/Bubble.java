12ac92c21393e8ba7c9bb5c57df48e279a334103
Ronald Lencevicius <rlencevicius@gmail.com>
2018-03-14 01:53:42 -0700

    Added swap method

--- src/algorithms/Bubble.java
+++ src/algorithms/Bubble.java
@@ -1,26 +1,28 @@
 package algorithms;
 
 public abstract class Bubble<T>{
-
-
     public static <T extends Comparable<T>> void sortI(T[] arr) {
         boolean swap;
+
         for (int i = 0; i < arr.length; i++) {
+            swap = false;
+
             for (int j = 0; j < arr.length; j++) {
-                swap = false;
                 if (arr[i].compareTo(arr[i + 1]) > 1) {
-                    T temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
+                    swap(arr[i] , arr[i + 1]);
                     swap = true;
                 }
+            }
 
-                if (swap == false) {
-                    break;
-                }
+            if (swap == false) {
+                break;
             }
         }
     }
 
-
+    private static <T> void swap(T val1 , T val2) {
+        T temp = val1;
+        val1 = val2;
+        val2 = temp;
+    }
 }
